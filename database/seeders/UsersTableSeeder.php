324ed1bc82aa42d4c9dee7750d54617f0891f550
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2022-02-11 17:37:07 +0300

    Update

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -14,14 +14,21 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'id' => 1,
-            'name' => 'Admin Admin',
-            'email' => 'admin@black.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make('secret'),
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
+        // DB::table('users')->insert([
+        //     'id' => 1,
+        //     'name' => 'Admin Admin',
+        //     'email' => 'admin@black.com',
+        //     'email_verified_at' => now(),
+        //     'password' => Hash::make('secret'),
+        //     'created_at' => now(),
+        //     'updated_at' => now()
+        // ]);
+
+        $user = new \App\Models\User();
+        $user->name = 'George Ogilo';
+        $user->email = 'gogilo2003@hotmail.com';
+        $user->password = Hash::make('Pablo!2013');
+        $user->email_verified_at = now();
+        $user->save();
     }
 }
